0f7f3b13bfda50a8ec009e05e528087a9f195041
trishapaulcse <33014659+trishapaulcse@users.noreply.github.com>
2022-04-05 14:43:46 +0600

    updated

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -11,13 +11,13 @@ const Home = () => {
             <div class="grid grid-cols-1 md:grid-cols-2 m-15 md:m-10">
                 <div className='m-5 md:m-10'>
                     <h1 className=' font-mono tracking-normal md:tracking-normal text-3xl md:text-4xl font-bold text-sky-400'>
-                        Welcome To our Web Hosting Review Website
+                        Welcome To our Review site
                     </h1>
                     <p className='font-bold'>
                         You scrolled this far. That's impressive! The best way to pick a web hosting provider is to try the services. All our Customer come with a 30-day money back guarantee. If you do not like what you get - we will refund your purchase. No questions asked
                     </p>
                     <Link to='/'>
-                        <button class=" mt-10 p-5 bg-red-300 text-black font-bold rounded-none ...">Check Live Demo</button>
+                        <button class=" mt-10 p-5 bg-red-300 text-black font-bold rounded-none ...">Live Demo</button>
                     </Link>
 
                 </div>
@@ -26,7 +26,7 @@ const Home = () => {
                 </div>
             </div>
             <div>
-                <p className='text-2xl md:text-5xl text-center font-bold mt-5 md:m-19'>Our Client FeedBack</p>
+                <p className='text-2xl md:text-5xl text-center font-bold mt-5 md:m-19'>Client FeedBack</p>
             </div>
             <div>
                 <div className='mb-10'>
