e2b62c036acd23c63d08f07607e19b757b7f98dd
trishapaulcse <33014659+trishapaulcse@users.noreply.github.com>
2022-04-05 15:30:14 +0600

    new update

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ import CustomLink from '../CustomLink/CustomLink';
 const Header = () => {
     return (
         <div className='sticky top-0 bg-white px-12 py-8 flex justify-center md:justify-between mx-auto md:mx-auto'>
-            <div className='hidden md:block'><Link to='/' className='font-sans font=bold text-xl text-gray-600'>PC-POINT-SHOP</Link></div>
+            <div className='hidden md:block'><Link to='/' className='font-sans font=bold text-xl text-gray-600'>PC-POINT-CENTER</Link></div>
             <div className='flex gap-4 text-xl'>
                 <CustomLink to='/'>Home</CustomLink>
                 <CustomLink to='/review'>Review</CustomLink>
